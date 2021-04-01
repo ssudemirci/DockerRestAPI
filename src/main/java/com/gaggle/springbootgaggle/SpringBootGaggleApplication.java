ff28d0a400e6a2f382beb621faa1efcc2e8d38e2
seljukqa <seljukqa@gmail.com>
2021-03-31 23:39:38 -0400

    ...

--- src/main/java/com/gaggle/springbootgaggle/SpringBootGaggleApplication.java
+++ src/main/java/com/gaggle/springbootgaggle/SpringBootGaggleApplication.java
@@ -1,10 +1,9 @@
 package com.gaggle.springbootgaggle;
 
-//import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//
 @SpringBootApplication
 public class SpringBootGaggleApplication {
 
